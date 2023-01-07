efed57ea41b619b9abd1aefd8b8fc47cf7af9d4d
yuz_97 <zhoumilau@gmail.com>
2023-01-07 16:16:59 +0800

    belajar vue2

---
+++ src/components/Card.vue
@@ -0,0 +1,11 @@
+<template>
+   <div>ini adalah card pada vue js</div>
+</template>
+
+<script>
+export default {
+   name: "CardComponent",
+};
+</script>
+
+<style></style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../view/Home.vue";
+import About from "../view/About.vue";
+
+Vue.use(VueRouter);
+const routes = [
+   {
+      path: "/",
+      name: "Home",
+      component: Home,
+   },
+   {
+      path: "/bout",
+      name: "About",
+      component: About,
+   },
+];
+
+const router = new VueRouter({
+   mode: "history",
+   base: process.env.BASE_URL,
+   routes,
+});
+
+export default router;
